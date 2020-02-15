11ebe51251d8026a1634921bce7b0bfc57a7a41b
Farid <fdmassuh@gmail.com>
2020-02-15 01:02:54 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Farid on 2/15/20.
+//  Copyright © 2020 Farid Massuh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
